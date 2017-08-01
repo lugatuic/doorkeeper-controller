cf997f48a3020e3ac31eada26bfbf05703f639ca
Curt <cthieme74@gmail.com>
2017-08-01 18:34:58 -0500

    file to open door

--- open_door.py
+++ open_door.py
@@ -3,6 +3,7 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(37, GPIO.OUT)
 GPIO.setup(38, GPIO.OUT)
+time.sleep(0.5)
 
 GPIO.output(37, GPIO.HIGH)
 GPIO.output(38, GPIO.HIGH)
@@ -11,4 +12,5 @@ time.sleep(5)
 GPIO.output(37, GPIO.LOW)
 GPIO.output(38, GPIO.LOW)
 
+time.sleep(1)
 GPIO.cleanup()
